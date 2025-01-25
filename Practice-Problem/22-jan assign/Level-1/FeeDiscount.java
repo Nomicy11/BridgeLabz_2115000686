40adb0df137843265f8d775d83bd98a441f18825
Nomicy11 <98690263+Nomicy11@users.noreply.github.com>
2025-01-25 23:52:36 +0530

    Delete Practice-Problem/22-jan assign directory

--- Practice-Problem/22-jan assign/Level-1/FeeDiscount.java
+++
@@ -1,9 +0,0 @@
-public class FeeDiscount{
-	public static void main(String[] args){
-		double fee = 125000;
-		double discountPercent = 10;
-		double discount = 125000*10/100;
-		double discountedFee = fee - discount;
-		System.out.println("The discount amount is INR " +discount +" and final discounted fee is INR " +discountedFee);
-	}
-}
